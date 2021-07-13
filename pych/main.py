9d3458df6aabc5ff4cfc2a3f6c5266032c85d137
Jostautas <jostautas.sakas@gmail.com>
2021-07-13 13:40:37 +0300

    laying foundaitions for GUI

--- pych/main.py
+++ pych/main.py
@@ -1,18 +1,21 @@
 import PySimpleGUI as sg
 
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-f = open('data.txt')
+
+file_name = 'data.txt'
+f = open(file_name)
 
 layout = [
-    [sg.Text(" " * 97), sg.Text("Month:", justification='center')],
-    [sg.Button("1. Jan", size=(15, 5)), sg.Button("2. Feb", size=(15, 5)), sg.Button("3. Mar", size=(15, 5)), sg.Button("4. Apr", size=(15, 5)), sg.Button("5. May", size=(15, 5)), sg.Button("6. Jun", size=(15, 5))],
-    [sg.Button("7. Jul", size=(15, 5)), sg.Button("8. Aug", size=(15, 5)), sg.Button("9. Sep", size=(15, 5)), sg.Button("10. Oct", size=(15, 5)), sg.Button("11. Nov", size=(15, 5)), sg.Button("12. Dec", size=(15, 5))],
-    [sg.Text("Enter or delete the name of the event and its day of month:")],
-    [sg.Multiline(f.read())]
-]
+        [sg.Text(" " * 97), sg.Text("Month:", justification='center')],
+        [sg.Button("1. Jan", size=(15, 5)), sg.Button("2. Feb", size=(15, 5)), sg.Button("3. Mar", size=(15, 5)), sg.Button("4. Apr", size=(15, 5)), sg.Button("5. May", size=(15, 5)), sg.Button("6. Jun", size=(15, 5))],
+        [sg.Button("7. Jul", size=(15, 5)), sg.Button("8. Aug", size=(15, 5)), sg.Button("9. Sep", size=(15, 5)), sg.Button("10. Oct", size=(15, 5)), sg.Button("11. Nov", size=(15, 5)), sg.Button("12. Dec", size=(15, 5))],
+        [sg.Text("Enter or delete the name of the event and its day of month:")],
+        [sg.Multiline(f.read(), key ="override")]
+    ]
 
 window = sg.Window("annual reminder", layout, size=(880, 400))
 
@@ -20,8 +23,10 @@ if __name__ == '__main__':
     while True:
         event, values = window.read()
         if event:
-            print(event)
-        else:
-            if event == sg.WIN_CLOSED:
-                break
+            file_name = "/home/jostautas/Documents/gits/gimtadieniai/duom_failai/PVZ.txt"
+            f = open(file_name)
+            window["override"].Update(f.read())
+            # print(values[0])
+        elif event == sg.WIN_CLOSED:
+            break
     window.close()
