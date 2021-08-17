85416f9690adb8ea5164c541fff77db7e6312156
Jostautas <jostautas.sakas@gmail.com>
2021-08-17 12:06:08 +0300

    changed example file

--- GUI_and_data/main.py
+++ GUI_and_data/main.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 from subprocess import call
 
 
-file_name = "data_files/PVZ.txt"
+file_name = "data_files/example.txt"
 f = open(file_name)
 
 months = ["1. January", "2. February", "3. March", "4. April", "5. May", "6. June", "7. July", "8. August", "9. September", "10. October", "11. November", "12. December"]  # is used in buttons
